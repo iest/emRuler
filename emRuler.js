f789cecf32c88afe1ee18c574e4e0814db0b24ab
Iestyn Williams <Iestyn.Williams@eysys.com>
2013-06-26 14:30:10 +0100

    Togglable

--- emRuler.js
+++ emRuler.js
@@ -1,18 +1,24 @@
-var bodyFontSize = 16; // Define the body font-size of your browser
+if (document.querySelector('.emRuler')) {
+    var oldEmRuler = document.querySelector('.emRuler');
+    oldEmRuler.parentNode.removeChild(oldEmRuler);
+}
+else {
+    var bodyFontSize = 16; // Define the body font-size of your browser
 
-var d; // Define the ruler itself first
+    var emRuler; // Define the ruler itself first
 
+    emRuler = document.createElement('div'); // Create the ruler div
 
-window.onload = function() { //... now the DOM is ready:
+    emRuler.classList.add('emRuler');
 
-    d = document.createElement('div'); // Create a the ruler div
+    document.body.appendChild(emRuler); // Now attach it to the document body
 
-    d.classList.add('widthCalculator'); // Add a class to the element, so we can target it with CSS
+    var style = "position:fixed;bottom:0;right:0;background:#1abc9c;color:#34495e;padding:.5em;font-family:'Futura'"
 
-    document.body.appendChild(d); // Now attach it to the document body
+    emRuler.setAttribute('style', style);
 
     function toEms(width) { // A simple function to convert to ems
-        d.innerHTML = Math.round(width / bodyFontSize) + 'em'; // Set the text of the ruler to the width
+        emRuler.innerHTML = Math.round(width / bodyFontSize) + 'em'; // Set the text of the ruler to the width
     }
 
 
@@ -24,4 +30,4 @@ window.onload = function() { //... now the DOM is ready:
         width = document.body.offsetWidth;
         toEms(width);
     };
-};
+}
