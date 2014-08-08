f6bf67b9a3756b17e409040c721ddf93ec0d1c72
Iestyn Williams <iest@me.com>
2014-08-08 14:52:07 +0100

    add vertical ruler on click

--- emRuler.js
+++ emRuler.js
@@ -1,5 +1,8 @@
+var isVertical = false;
+
 if (document.querySelector('.emRuler')) {
     var oldEmRuler = document.querySelector('.emRuler');
+    window.removeEventListener('resize', calculate);
     oldEmRuler.parentNode.removeChild(oldEmRuler);
 } else {
     var bodyFontSize = 16; // Define the body font-size of your browser
@@ -12,7 +15,7 @@ if (document.querySelector('.emRuler')) {
 
     document.body.appendChild(emRuler); // Now attach it to the document body
 
-    var style =
+    var horizontalStyle =
         "position: fixed;" +
         "z-index: 9999999999;" +
         "bottom: 0;" +
@@ -25,19 +28,51 @@ if (document.querySelector('.emRuler')) {
         "text-align: center;" +
         "font-family: 'Futura'";
 
-    emRuler.setAttribute('style', style);
+    var verticalStyle =
+        "position: fixed;" +
+        "z-index: 9999999999;" +
+        "right: 0;" +
+        "top: 0;" +
+        "bottom: 0;" +
+        "background: #1abc9c;" +
+        "border-left: 1px solid rgba(255,255,255,0.5);" +
+        "color: #34495e;" +
+        "padding: 0.5em;" +
+        "text-align: center;" +
+        "font-family: 'Futura'";
 
-    function toEms(width) { // A simple function to convert to ems
-        emRuler.innerHTML = Math.round(width / bodyFontSize) + 'em'; // Set the text of the ruler to the width
+    emRuler.setAttribute('style', horizontalStyle);
+
+    var width = window.innerWidth; // Grab the viewport width
+    var height = window.innerHeight; // Grab the viewport height
+
+    setText(width); // Now actually set the text of the ruler
+
+    window.addEventListener('resize', calculate);
+
+    emRuler.addEventListener('click', function() {
+        if (isVertical) {
+            isVertical = false;
+            emRuler.setAttribute('style', horizontalStyle);
+        } else {
+            isVertical = true;
+            emRuler.setAttribute('style', verticalStyle);
+        }
+        calculate();
+    });
+}
+
+
+function setText(pixels) { // A simple function to convert to ems
+    emRuler.innerHTML = Math.round(pixels / bodyFontSize) + 'em';
+}
+
+function calculate() { // Setup a function on the resize event so it updates as you scale the browser
+    width = window.innerWidth; // Grab the viewport width
+    height = window.innerHeight; // Grab the viewport height
+    if (isVertical) {
+        setText(height);
+    } else {
+        setText(width);
     }
-
-
-    var width = document.body.offsetWidth; // Grab the document width
-
-    toEms(width); // Now actually set the text of the ruler
-
-    window.onresize = function() { // Setup a function on the resize event so it updates as you scale the browser
-        width = document.body.offsetWidth;
-        toEms(width);
-    };
 }
